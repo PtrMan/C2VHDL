718846267378e2d1e1470d05fae68226cfc41c1d
PtrMan <robertw89@googlemail.com>
2013-10-15 22:37:52 +0200

    style, doxygen comments

--- compiler/exceptions.py
+++ compiler/exceptions.py
@@ -5,7 +5,7 @@ __version__ = "0.1"
 import os
 
 class C2CHIPError(Exception):
-    def __init__(self, message, filename=None, lineno=None):
-        self.message = message
-        self.filename = os.path.abspath(filename)
-        self.lineno = lineno
+	def __init__(self, message, filename=None, lineno=None):
+      self.message = message
+      elf.filename = os.path.abspath(filename)
+      self.lineno = lineno
